67b904f6791d6ddb6adb27ac414bc3e7df405a42
christinabeletskaya <christina.b@turing.com>
2025-11-07 00:59:23 +0200

    fix cronjob

--- booking_api_service/main.py
+++ booking_api_service/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 )
 
 
-@app.websocket("/api/handle_call")
+@app.websocket('/media-stream')
 async def handle_call(websocket: WebSocket):
     await websocket.accept()
     user_id = websocket.query_params.get("user_id")
@@ -26,14 +26,26 @@ async def handle_call(websocket: WebSocket):
 @app.get("/api/call-orchestrator", response_class=JSONResponse)
 async def call_orchestrator():
     phone_number = "+97233824145"
-    client.calls.create(
-        from_=phone_number,
-        to="+972527500553",
-        twiml=(
-            f'<?xml version="1.0" encoding="UTF-8"?>'
-            f'<Response><Connect><Stream url="wss://callflow-rho.vercel.app/api/handle_call?user_id=690c8b7ddc1c8ec2a78af495&meeting_request_id=690ccd5d49a650787e3b1323"/></Connect></Response>'
-        )
+
+    outbound_twiml = (
+        f'<?xml version="1.0" encoding="UTF-8"?>'
+        f'<Response><Connect><Stream url="wss://callflow-rho.vercel.app/media-stream" /></Connect></Response>'
     )
+    call = client.calls.create(
+        from_="+97233824145",
+        to="+972527500553",
+        twiml=outbound_twiml
+    )
+    print(f"Call started with SID: {call.sid}")
+
+    # client.calls.create(
+    #     from_=phone_number,
+    #     to="+972527500553",
+    #     twiml=(
+    #         f'<?xml version="1.0" encoding="UTF-8"?>'
+    #         f'<Response><Connect><Stream url="wss://callflow-rho.vercel.app/api/handle_call?user_id=690c8b7ddc1c8ec2a78af495&meeting_request_id=690ccd5d49a650787e3b1323"/></Connect></Response>'
+    #     )
+    # )
     # # figure out what phone numbers are available
     # # if no phone numbers available, do nothing
     # MEETING_REQUEST_QUEUE = []
