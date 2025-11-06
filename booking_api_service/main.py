78fa84d3fc3a8c23809a945c50128823cf7fa535
christinabeletskaya <christina.b@turing.com>
2025-11-06 02:56:36 +0200

    first commit

---
+++ booking_api_service/main.py
@@ -0,0 +1,26 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from twilio.rest import Client
+
+client = Client(
+    os.getenv("TWILIO_ACCOUNT_SID"),
+    os.getenv("TWILIO_AUTH_TOKEN"),
+)
+
+app = FastAPI(
+    title="Booking API Service",
+    version="1.0.0",
+)
+
+
+@app.post("/api/book-meeting", response_class=JSONResponse)
+async def book_meeting():
+    phone_number = "+97233824145"
+    call = client.calls.create(
+        from_=phone_number,
+        to="+972527500553",
+        twiml="<Response><Say>Hello from Twilio!</Say></Response>"
+    )
+    return {"message": f"{call.sid}"}
