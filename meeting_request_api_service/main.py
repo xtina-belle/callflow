612fbaa6f6a5bd81e4bc487224f4385af886ecb1
christinabeletskaya <christina.b@turing.com>
2025-11-07 02:42:28 +0200

    fix cronjob

--- meeting_request_api_service/main.py
+++ meeting_request_api_service/main.py
@@ -10,7 +10,7 @@ from service import meeting_agent
 client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
 
 app = FastAPI(
-    title="Booking API Service",
+    title="Meeting Requests API Service",
     version="1.0.0",
 )
 
@@ -18,18 +18,16 @@ app = FastAPI(
 @app.websocket('/media-stream')
 async def handle_call(websocket: WebSocket):
     await websocket.accept()
-    user_id = websocket.query_params.get("userId")
-    meeting_request_id = websocket.query_params.get("meeting_request_id")
+    user_id = websocket.query_params["userId"]
+    meeting_request_id = websocket.query_params["meetingRequestId"]
     await meeting_agent.handle_meeting_request_call(user_id, meeting_request_id, websocket)
 
 
 @app.get("/api/call-orchestrator", response_class=JSONResponse)
 async def call_orchestrator():
-    phone_number = "+97233824145"
-
     outbound_twiml = (
         f'<?xml version="1.0" encoding="UTF-8"?>'
-        f'<Response><Connect><Stream url="wss://bountiful-cat-production.up.railway.app/media-stream?userId=690c8b7ddc1c8ec2a78af495" /></Connect></Response>'
+        f'<Response><Connect><Stream url="wss://bountiful-cat-production.up.railway.app/media-stream?userId=690c8b7ddc1c8ec2a78af495&meeting_request_id=690ccd5d49a650787e3b1323" /></Connect></Response>'
     )
     call = client.calls.create(
         from_="+97233824145",
@@ -38,14 +36,6 @@ async def call_orchestrator():
     )
     print(f"Call started with SID: {call.sid}")
 
-    # client.calls.create(
-    #     from_=phone_number,
-    #     to="+972527500553",
-    #     twiml=(
-    #         f'<?xml version="1.0" encoding="UTF-8"?>'
-    #         f'<Response><Connect><Stream url="wss://bountiful-cat-production.up.railway.app/api/handle_call?user_id=690c8b7ddc1c8ec2a78af495&meeting_request_id=690ccd5d49a650787e3b1323"/></Connect></Response>'
-    #     )
-    # )
     # # figure out what phone numbers are available
     # # if no phone numbers available, do nothing
     # MEETING_REQUEST_QUEUE = []
