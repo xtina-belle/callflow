270ffa56bcd452da5afa8aadb577c31980eca2c5
christinabeletskaya <krisbeletskaya@gmail.com>
2026-02-18 19:10:48 +0200

    next fix

--- fe/src/middleware.ts
+++ fe/src/middleware.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 
 import authConfig from "@/app/auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig)
+export const { auth } = NextAuth(authConfig);
 
-export const config = { matcher: ["/", "/login"] }
+export default auth;
+
+export const config = { matcher: ["/", "/login"] };
